1dcf26c75b21edee1dd20a90886748782a9cc205
Galdin.Ilya <mymainwilliw@gmail.com>
2016-11-05 15:49:04 +0300

    [comment] Добавлены комментарии + некие проверки

--- HLyaa/Controllers/EventsController.cs
+++ HLyaa/Controllers/EventsController.cs
@@ -40,6 +40,8 @@ namespace HLyaa.Controllers
     {
       CreateNewEventModel model = new CreateNewEventModel();
       var usersList = db.UsersInfo.OrderBy(m => m.Name);
+      // Передаём в модель днанных выводимую информацию: id пользователя,
+      // имя пользователя, и сумма взноса (равна 0).
       foreach (var user in usersList)
       {
         model.BuyerDataItems.Add(new BuyerDataItem()
@@ -57,6 +59,7 @@ namespace HLyaa.Controllers
     {
       if (ModelState.IsValid)
       {
+        // Создаем новое событие
         var newEvent = new Event()
         {
           Name = model.EventName,
@@ -64,12 +67,17 @@ namespace HLyaa.Controllers
           DateCreated = DateTime.Now,
           Reporter = userHelper.CurrentUserInfo()
         };
+        // получаем в newEvent новый Id 
         newEvent = db.Events.Add(newEvent);
 
+        // Сразу создаем информацию о взносе и информацию о долге.
+        // Информация о долге нужна для корректной работы системы:
+        // "сумма всех операций должна быть равна 0 (или близка к ней)"
         foreach (var item in model.BuyerDataItems)
         {
           if (item.Data > 0)
           {
+            // Положительная операция (взнос)
             db.DebtParts.Add(new DebtPart()
             {
               Part = null,
@@ -78,6 +86,7 @@ namespace HLyaa.Controllers
               Event = newEvent,
               User = db.UsersInfo.Find(item.UserId)
             });
+            // Отрицательная операция (долг)
             db.DebtParts.Add(new DebtPart()
             {
               Part = null,
@@ -88,6 +97,7 @@ namespace HLyaa.Controllers
             });
           }
         }
+        // Попытка запихать все в БД
         try
         {
           db.SaveChanges();
@@ -107,14 +117,24 @@ namespace HLyaa.Controllers
 
 
     //GET: Events/SetDebt
+    /// <summary>
+    /// GET запрос метода выбора должников
+    /// </summary>
+    /// <param name="eventId"> Идентификатор события</param>
     public ActionResult SetDebt(int eventId)
     {
+      // Работаем с событием с идентификатором eventId
       SetDebtModel model = new SetDebtModel() { EventId = eventId };
+      // Получаем сисок всех пользователей
       var usersList = db.UsersInfo.OrderBy(m => m.Name).ToList();
+      // Заполняем модель данных
       foreach (var user in usersList)
-      {        
+      {
+        // Ищем записи в таблице операций с отрицательным значением суммы
         var debtor = db.DebtParts.SingleOrDefault(m => m.EventId == eventId &&
           m.UserId == user.Id && ((m.Part > 0) || (m.Summ < 0)));
+        // Если найдено, то переменная selected будет равно true,
+        // это значение перейдет в CheckBox
         bool selected = (debtor != null);
         model.DebtorCoiseItems.Add(new DebtorCoiseItem()
         {
@@ -126,6 +146,12 @@ namespace HLyaa.Controllers
       return View(model);
     }
     //POST: Events/SetDebt
+    /// <summary>
+    /// POST запрос метода выбора должников.
+    /// В модель приходит список выбранных пользователей
+    /// </summary>
+    /// <param name="model"> Модель данных</param>
+    /// <returns></returns>
     [HttpPost]
     public ActionResult SetDebt(SetDebtModel model)
     {
@@ -133,13 +159,16 @@ namespace HLyaa.Controllers
       bool warningFlag = true;
       foreach(var item in model.DebtorCoiseItems)
       {
-        
+        // Если пользователь не выбран - очищаем его отрицательные операции этого события
         if (item.Selected == false)
         {
+          // Ищем отрицательные операции этого события не выбранного пользователя
           var list = db.DebtParts.Where(m => m.EventId == model.EventId &&
             m.UserId == item.UserId && ((m.Part > 0) || (m.Summ < 0))).ToList();
 
+          // Удаляем из контекста
           db.DebtParts.RemoveRange(list);
+          // Пробуем удалить из БД
           try
           {
             db.SaveChanges();
@@ -153,45 +182,35 @@ namespace HLyaa.Controllers
         }
         else
         {
+          // Если пользователь выбран Заполняем выходную строку пользователей
           retList += item.UserId.ToString() + ",";
           warningFlag = false;
         }
       }
       
+      //Если Ни один пользователь не выбран - ничего не происходит
       if(warningFlag)
       {
         return View(model);
       }
-      if (retList.Count() > 0)
+      else
       {
+        // Убираем последнюю запятую из списка пользователей
         retList = retList.Substring(0, retList.Length - 1);
       }
-      var test = retList.Split(',').Select(int.Parse).ToList();
       return RedirectToAction("SetPartAndPrice", "Events", new { userList = retList, eventId = model.EventId });
     }
 
-    public class IntListModelBinder : DefaultModelBinder
-    {
-      public override object BindModel(ControllerContext controllerContext, ModelBindingContext bindingContext)
-      {
-        var value = bindingContext.ValueProvider.GetValue(bindingContext.ModelName);
-        if (value == null || string.IsNullOrEmpty(value.AttemptedValue))
-        {
-          return null;
-        }
-
-        return value
-            .AttemptedValue
-            .Split(',')
-            .Select(int.Parse)
-            .ToList();
-      }
-    }
 
     //GET: Events/SetPartAndPrice
+    /// <summary>
+    /// GET запрос метода выставления долгов
+    /// </summary>
+    /// <param name="userList"> Список выбранных должников</param>
+    /// <param name="eventId"> Идентификатор события</param>
+    /// <returns></returns>
     public ActionResult SetPartAndPrice([ModelBinder(typeof(IntListModelBinder))]List<int> userList, int eventId)
-    {
-      
+    {      
       SetPartModel model = new SetPartModel() { EventId = eventId };
       var usersList = db.UsersInfo.OrderBy(m => m.Name).ToList();
       foreach (var user in usersList)
@@ -210,18 +229,23 @@ namespace HLyaa.Controllers
       return View(model);
     }
     //POST: Events/SetPartAndPrice
+    /// <summary>
+    /// POST запрос метода выставления долгов
+    /// </summary>
+    /// <param name="model"> Модель данных</param>
+    /// <returns></returns>
     [HttpPost]
     public ActionResult SetPartAndPrice(SetPartModel model)
     {
-      int debtPartCount = 0;
+      // Начало расчета долгов
       double debtSumSum = 0;
       double debtPartSum = 0;
 
+      // Считаем сумму всех частей долгов и сумму всех денежных долгов
       foreach (var item in model.DebtorDataItems)
       {        
         if (item.DebtPart > 0)
         {
-          ++debtPartCount;
           debtPartSum += item.DebtPart;
         }
         else if (item.DebtSum > 0)
@@ -230,20 +254,26 @@ namespace HLyaa.Controllers
         }
       }
 
+      // Считаем общую сумму взноса (положительных операций)
       double globalSum = db.DebtParts.Where(m => m.EventId == model.EventId && m.Summ > 0).Sum(m => m.Summ);
-      if(debtSumSum >globalSum)
+      double diffSum = globalSum - debtSumSum;
+      // Проверка на не корректные значения
+      if ( (debtPartSum == 0 && diffSum > 1) || (debtSumSum > globalSum) )
       {
         return View(model);
       }
-
-      double calcSum = globalSum - debtSumSum;
+      // Расчет долга для каждого пользователя
       foreach (var item in model.DebtorDataItems)
       {
+        // Ищем: есть ли запись о долге для этого пользователя в этом событии
         var debtor = db.DebtParts.SingleOrDefault(m => m.EventId == model.EventId &&
           m.UserId == item.UserId && ((m.Part > 0) || (m.Summ < 0)));
-        if (item.DebtPart > 0)
-        {
-          double realSum = (item.DebtPart * calcSum) / debtPartSum;
+        // Если у пользовател указана "часть"
+        if (item.DebtPart > 0)        {
+          
+          // Считаем сумму долга для пользователя
+          double realSum = (item.DebtPart * diffSum) / debtPartSum;
+          // Если нет пользователя - создаем
           if (debtor == null)
           {
             debtor = db.DebtParts.Add(new DebtPart()
@@ -255,14 +285,17 @@ namespace HLyaa.Controllers
               UserId = item.UserId
             });
           }
+          // Иначе - изменяем
           else
           {
             debtor.Part = item.DebtPart;
             debtor.Summ = -realSum;
           }
         }
-        else if(item.DebtSum > 0)
-        {          
+        // Если у пользовател указана "сумма"
+        else if (item.DebtSum > 0)
+        {
+          // Если нет пользователя - создаем
           if (debtor == null)
           {
             debtor = db.DebtParts.Add(new DebtPart()
@@ -274,6 +307,7 @@ namespace HLyaa.Controllers
               UserId = item.UserId
             });
           }
+          // Иначе - изменяем
           else
           {
             debtor.Part = null;
@@ -281,7 +315,8 @@ namespace HLyaa.Controllers
           }
         }
       }
-
+      
+      // Попытка записи в БД
       try
       {
         db.SaveChanges();
@@ -293,6 +328,7 @@ namespace HLyaa.Controllers
         logger.Error(String.Format("Compleate create event {0}", model.EventId));
         return View(model);
       }
+      // Проверка созданнгогог события на корректность сумм
       double test2 = db.DebtParts.Where(m => m.EventId == model.EventId).Sum(m => m.Summ);
       if (test2 > -0.005 && test2 < 0.005)
       {
